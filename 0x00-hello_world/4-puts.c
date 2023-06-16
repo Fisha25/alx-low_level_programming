37fca9b37e342163174281f49b2bc06fc9a356db
Fisha25 <fishasse38@gmail.com>
2023-06-15 18:16:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses put function to print a line
+ * Return: 0 ( done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+return (0);
+}
