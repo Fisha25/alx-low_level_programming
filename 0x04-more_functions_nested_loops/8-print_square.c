e8f24852a42fc7bda476632dba7afd535a7c05ee
Fisha25 <fishasse38@gmail.com>
2023-06-23 18:39:54 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,22 +7,22 @@
  */
 void print_square(int size)
 {
-int col, row;
+	int col,row;
 
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (col = 1; col <= size; col++)
-{
-_putchar('#');
-for (row = 2; row <= size; ro++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
+	if (size<=0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (col=1;col<=size;co++)
+		{
+			_putchar('#');
+			for (row=2; row<=size;row++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
