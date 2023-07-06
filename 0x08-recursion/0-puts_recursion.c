141a6b19a6e9611f9cda0ae2494f49ec0e25134f
Fisha25 <fishasse38@gmail.com>
2023-07-06 14:31:34 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,13 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-void _puts_recursion(char *s)
+void _puts_recursion(char*s)
 {
-	if (*s == '\0')
+	if (*s=='\0')
 		_putchar('\n');
 	else
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(s+1);
 	}
 }
